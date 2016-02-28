1aa5393c65cdfbd9b1fdd2a4f9a26da86668b64c
Deniz Ugurlu <mail@denizugurlu.de>
2016-02-28 14:35:44 +0100

    added markdown dependency; added markdown template;

--- src/js/components/Preview.js
+++ src/js/components/Preview.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Preview extends React.Component {
   render() {
     return (
-      <span>{this.props.markdown}</span>
+      <span dangerouslySetInnerHTML={this.props.markdown}></span>
     );
   }
 }
